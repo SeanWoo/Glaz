095fb05dbd97dd9fb52d460ca7cd8b9271cb9bba
Stas Khavruk (Jeidoz) <stas_khavruk@outlook.com>
2020-08-05 23:38:58 +0300

    Update database to new account

---
+++ Glaz.Server/Migrations/20200805203811_UseCustomAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Glaz.Server.Migrations
+{
+    public partial class UseCustomAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                maxLength: 128,
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBanned",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                maxLength: 128,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegisteredAt",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsBanned",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RegisteredAt",
+                table: "AspNetUsers");
+        }
+    }
+}
