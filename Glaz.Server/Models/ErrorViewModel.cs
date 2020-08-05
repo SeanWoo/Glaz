ac00b9d374838c4b0ea8f4c4b41b895c09f19c03
Stas Khavruk (Jeidoz) <stas_khavruk@outlook.com>
2020-08-05 23:25:01 +0300

    ASP.NET Core MVC project init

---
+++ Glaz.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Glaz.Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
