356d2d54b37fde1324ed9aa4c09db0edac4cd325
sjaks <kokarevvadim2002@gmail.com>
2020-08-07 16:40:51 +0400

    Added API with video streaming and ping.

---
+++ Glaz.Server/Controllers/Api/PingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Glaz.Server.Controllers.Api
+{
+    [Route("api/Ping")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        public IActionResult Ping() => Ok();
+    }
+}
