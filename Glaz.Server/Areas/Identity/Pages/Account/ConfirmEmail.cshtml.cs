9c834979c3fd138505f954a8164a526fe930cc48
Stas Khavruk (Jeidoz) <stas_khavruk@outlook.com>
2020-08-13 04:14:18 +0300

    Complete translate of email ConfirmEmail.cshtml

--- Glaz.Server/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ Glaz.Server/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -37,7 +37,7 @@ namespace Glaz.Server.Areas.Identity.Pages.Account
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            StatusMessage = result.Succeeded ? "Спасибо что подтвердили свой email." : "Произошла ошибка подтверждения email-а.";
             return Page();
         }
     }
