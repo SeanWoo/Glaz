2438703273eb34fa6e464eddca1cc5965a7cfeb0
Stas Khavruk (Jeidoz) <stas_khavruk@outlook.com>
2020-08-18 20:54:11 +0300

    Add unit test and PingController coverage

---
+++ Glaz.Server.UnitTests/Controllers/Api/PingControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Glaz.Server.Controllers.Api;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Glaz.Server.UnitTests.Controllers.Api
+{
+    public sealed class PingControllerTests
+    {
+        [Fact]
+        public void Index_SimpleRequest_ReturnsOkResponse()
+        {
+            var controller = new PingController();
+
+            var actual = controller.Index();
+            
+            Assert.IsType<OkResult>(actual);
+        }
+    }
+}
