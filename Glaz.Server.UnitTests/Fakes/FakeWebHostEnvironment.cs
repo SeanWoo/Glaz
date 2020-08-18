bb504ffd4e3ac95517aae16deb11c713f2d5e874
Stas Khavruk (Jeidoz) <stas_khavruk@outlook.com>
2020-08-18 21:13:15 +0300

    Add StreamController.cs tests coverage

---
+++ Glaz.Server.UnitTests/Fakes/FakeWebHostEnvironment.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
+
+namespace Glaz.Server.UnitTests.Fakes
+{
+    public sealed class FakeWebHostEnvironment : IWebHostEnvironment
+    {
+        public string ApplicationName { get; set; }
+        public IFileProvider ContentRootFileProvider { get; set; }
+        public string ContentRootPath { get; set; }
+        public string EnvironmentName { get; set; }
+        public IFileProvider WebRootFileProvider { get; set; }
+        public string WebRootPath { get; set; } = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+    }
+}
